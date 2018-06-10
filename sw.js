c9e8a74649ee329f371bf508bf8b3c6b11109333
Shady Selim <Shady.Selim@hotmail.com>
2018-06-10 03:17:40 +0200

    Project Review Changes

--- sw.js
+++ sw.js
@@ -24,10 +24,6 @@ self.addEventListener('install', e=> {
     );
 });
 
-// self.addEventListener('activate',  event => {
-//     event.waitUntil(self.clients.claim());
-// });
-
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request, {ignoreSearch:true}).then(response => {
